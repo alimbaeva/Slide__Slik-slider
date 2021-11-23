1088325d059743628c7c28b1200dd78c7a5d967e
Asel Alimbaeva <asel_19.86@mail.ru>
2021-11-23 07:46:12 +0300

    work with slick library and added codes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.slides-wrapper').slick({
+        prevArrow: `<button class="btn btn-prev"><img src="img/keyboard-left-arrow-button_icon-icons.com_72692.svg" alt=""></button>`,
+        nextArrow: `<button class="btn btn-next"><img src="img/keyboard-right-arrow-button-1_icon-icons.com_72690.svg" alt=""></button>`,
+        dots: true,
+    });
+});
